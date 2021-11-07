44401d6eea3bbe725f01a426ee9acacf917ceca2
Falliot <pustovidko.anton@gmail.com>
2021-11-07 12:48:59 +0100

    Initial Commit

---
+++ AutoMaster/AutoMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoMasterApp.swift
+//  AutoMaster
+//
+//  Created by Anton on 07.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct AutoMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
