961c96087405c2212407ee18e899fda60c42867c
Falliot <pustovidko.anton@gmail.com>
2021-11-17 20:46:34 +0100

    create enums

--- AutoMaster/Utilities/Enums/Enums.swift
+++ AutoMaster/Utilities/Enums/Enums.swift
@@ -6,3 +6,58 @@
 //
 
 import Foundation
+
+enum BodyType {
+    case compact, sedan, stationWagon, convertible, coupe, pickUp, van, transporter
+}
+
+enum AutoCondition {
+    case any, new, used
+}
+
+enum Transmission {
+    case manual, automatic, semiAutomatic
+}
+
+enum FuelType {
+    case gasoline, diesel, electirc, electricGasoline, electricDiesel, cng, ethanol, hydroge, lpg, others
+}
+
+enum Equipment {
+    enum Comfort {
+        case airConditioning, powerWindows, splitRearSeats, tintedWindows
+    }
+    
+    enum Entertainment {
+        case cdPlayer, radio
+    }
+    
+    enum Safety {
+        case abs, centralDoorLock, driverSideAirbag, fogLights, immobilizer, isofix, passengerSideAirbag, powerSteering, sideAirbag
+    }
+    
+    enum Extras {
+        case catalyticConverter, steelWheels, winterTyres
+    }
+}
+
+enum ColorAndUpholstery {
+    enum InteriorFitting { //Upholstery
+        case any, alcantara, cloth, fullLeather, partLeather, velour, other
+    }
+    
+    enum PaintWork {
+        
+    }
+    
+    enum BodyColor {
+        case any, beige, black, blue, bronze, brown, gold, green, grey, orange, red, silver, violet, white, yellow
+        case metalic
+        // metallic ?
+        // alloy wheels ? (In Paint, RIMS)
+    }
+    
+    enum InteriorColor {
+        case any, beige, black, blue, brown, green, grey, orange, red, white, yellow, other
+    }
+}
