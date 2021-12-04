4c4b463f5b31c8dcffce367f703ab0af851843c3
Falliot <pustovidko.anton@gmail.com>
2021-12-04 10:50:06 +0100

    create isLiked, addToLiked, deleteFavorites methods

--- AutoMaster/Utilities/ViewModels/SharedDataModel.swift
+++ AutoMaster/Utilities/ViewModels/SharedDataModel.swift
@@ -19,4 +19,30 @@ class SharedDataModel: ObservableObject {
             objectWillChange.send()
         }
     }
+    
+    func isLiked(_ currentTransport: Transport) -> Bool {
+        return likedTransports.contains { transport in
+            return currentTransport.id == transport.id
+        }
+    }
+    
+    func addToLiked(_ currentTransport: Transport) {
+        if let index =  likedTransports.firstIndex(where: { transport in
+            return currentTransport.id == transport.id
+        }) {
+             likedTransports.remove(at: index)
+        } else {
+             likedTransports.append(currentTransport)
+        }
+    }
+    
+    func deleteFavorite(_ currentTransport: Transport) {
+        if let index = likedTransports.firstIndex(where: { selectedTransport in
+            return currentTransport.id == selectedTransport.id
+        }) {
+            let _ = withAnimation {
+                likedTransports.remove(at: index)
+            }
+        }
+    }
 }
