e54a8ac3f122d18eac8de6c8ff436d76b71a84e7
Falliot <pustovidko.anton@gmail.com>
2021-12-06 20:40:01 +0100

    clean code, wip

--- AutoMaster/Utilities/Constants/Constants.swift
+++ AutoMaster/Utilities/Constants/Constants.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct Constants {
+    struct Colors {
+        static let any = (Color.clear, "Any")
+        static let beige = (Color(red: 245, green: 245, blue: 220), "Beige")
+        static let black = (Color.black, "Black")
+        static let blue = (Color.blue, "Blue")
+        static let bronze = (Color(red: 205, green: 127, blue: 50), "Bronze")
+        static let brown = (Color.brown, "Brown")
+        static let gold = (Color(red: 255, green: 215, blue: 0), "Gold")
+        static let green = (Color.green, "Green")
+        static let gray = (Color.gray, "Gray")
+        static let orange = (Color.orange, "Orange")
+        static let red = (Color.red, "Red")
+        static let silver = (Color(red: 192, green: 192, blue: 192), "Silver")
+        static let violet = (Color(red: 143, green: 0, blue: 255), "Violet")
+        static let white = (Color.white, "White")
+        static let yellow = (Color.yellow, "Yellow")
+    }
+}
