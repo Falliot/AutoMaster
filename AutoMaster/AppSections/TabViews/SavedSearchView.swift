e7f26cd691e339c75225f58713700343197dfe11
Falliot <pustovidko.anton@gmail.com>
2021-12-22 17:59:33 +0100

    wip on savedSearchView

--- AutoMaster/AppSections/TabViews/SavedSearchView.swift
+++ AutoMaster/AppSections/TabViews/SavedSearchView.swift
@@ -8,21 +8,61 @@
 import SwiftUI
 
 struct SavedSearchView: View {
-    @ObservedObject var viewModel = SavedSearchViewModel()
-
+//    @EnvironmentObject var sharedData: SharedDataModel
+    
     var body: some View {
-        VStack(alignment: .center, spacing: 5) {
-            Image("searchCar")
-                .resizable()
-                .frame(width: 250, height: 250, alignment: .center)
-            Text("No saved searches")
-                .font(.system(size: 25, weight: .semibold, design: .rounded))
+        VStack(spacing: 0) {
+            HStack {
+                Text("Saved searches")
+                    .font(.system(size: 23, weight: .bold, design: .rounded))
+                    .foregroundColor(.white)
+                
+                Spacer()
+            }
+            .padding(10)
+            .padding(.horizontal, 15)
+            .padding(.bottom, 10)
+            .background(Color("Green"))
+            
+            ScrollView(.vertical, showsIndicators: false) {
+                VStack {
+//                    if sharedData.likedTransports.isEmpty {
+                        Group {
+                            Image("noSavedSearch")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .padding()
+                                .padding(.top, 35)
+                            
+                            Text("No saved search yet")
+                                .font(.system(size: 25, weight: .semibold, design: .rounded))
+                                .foregroundColor(Color("Green"))
+                            
+                            Text("Hit the star button after search search to save favorite ones.")
+                                .font(.system(size: 18, weight: .regular, design: .rounded))
+                                .foregroundColor(.gray)
+                                .padding(.horizontal, 50)
+                                .padding(.top, 10)
+                                .multilineTextAlignment(.center)
+                        }
+//                    }
+                }
+                
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(
+                Color("HomeBG")
+                    .ignoresSafeArea()
+            )
         }
     }
+    
 }
 
 struct SavedSearchView_Previews: PreviewProvider {
     static var previews: some View {
         SavedSearchView()
+            .environmentObject(SharedDataModel())
+            .previewDevice(PreviewDevice(rawValue: "iPhone 11"))
     }
 }
